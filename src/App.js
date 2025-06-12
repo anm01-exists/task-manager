4dd7a2460957a2b7fa099f948ffba1258194e8c2
likhitha_shree <likhithashreelc@gmail.com>
2025-06-12 13:25:40 +0530

    Updated App.js with latest changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header for app">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,6 +18,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          \
         </a>
       </header>
     </div>
