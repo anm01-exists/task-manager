1e97eb19ce907f90d680f52b9ea6ef02284d989f
Anmol Pathak <anmolpathak022@gmail.com>
2025-06-12 18:58:42 +0530

    structured

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav style={{ padding: '10px', background: '#eee' }}>
+      <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
+      <Link to="/about">About</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
